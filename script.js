4800fdc0787ed22ce3c72e489ac036963a7f41df
Nirvan004 <Nirvanandb2004@gmail.com>
2025-11-09 18:12:58 -0500

    added global variables i think i will need

---
+++ script.js
@@ -0,0 +1,8 @@
+let posts = [];
+
+const postForm = document.getElementById('postForm');
+const postTitle = document.getElementById('postTitle');
+const postContent = document.getElementById('postContent');
+const titleError = document.getElementById('titleError');
+const contentError = document.getElementById('contentError');
+const postsList = document.getElementById('postsList');
