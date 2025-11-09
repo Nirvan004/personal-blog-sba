1a214b78c5863e03b920c4cb8d1a84e757154db3
Nirvan004 <Nirvanandb2004@gmail.com>
2025-11-09 18:23:26 -0500

    added the rendering posts function that will be called when adding and deleting posts as well as showing all posts from local storage

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ document.addEventListener('DOMContentLoaded', () => {
     renderPosts();
   }
 });
+
+function renderPosts() {
+  postsList.innerHTML = '';
+
+  posts.forEach(post => {
+    const postDiv = document.createElement('div');
+    postDiv.classList.add('post');
+    postDiv.setAttribute('data-id', post.id);
+
+    const postTitleEl = document.createElement('h3');
+    postTitleEl.textContent = post.title;
+
+    const postContentEl = document.createElement('p');
+    postContentEl.textContent = post.content;
+
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.classList.add('edit');
+    editBtn.setAttribute('data-id', post.id);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete');
+    deleteBtn.setAttribute('data-id', post.id);
+
+    postDiv.appendChild(postTitleEl);
+    postDiv.appendChild(postContentEl);
+    postDiv.appendChild(editBtn);
+    postDiv.appendChild(deleteBtn);
+
+    postsList.appendChild(postDiv);
+  });
+}
